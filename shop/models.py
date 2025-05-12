ef682fd8202ac6f89aa7b42522c9290fc0745189
ivansizov <sizovivan.05@gmail.com>
2025-05-13 02:05:00 +0700

    add products model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
