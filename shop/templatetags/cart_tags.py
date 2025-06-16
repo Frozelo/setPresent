62c76de856d9f883980d974894329caf28c81b8c
Ivan Sizov <127422441+Frozelo@users.noreply.github.com>
2025-06-16 19:24:22 +0700

    Implement order creation in cart

--- shop/templatetags/cart_tags.py
+++ shop/templatetags/cart_tags.py
@@ -10,3 +10,9 @@ def multiply(value1, value2):
 def sum_total(cart_items):
     total = sum(item.quantity * item.product_id.price for item in cart_items)
     return total
+
+
+@register.filter
+def sum_quantity(cart_items):
+    """Return the total quantity of products in the cart."""
+    return sum(item.quantity for item in cart_items)
